dee155a80069b596096950ff709b08eed93d73dc
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-05 18:49:31 -0400

    adds Workouts class and createWorkout function to push new workouts to backend

---
+++ fitness-frontend/src/components/workouts.js
@@ -0,0 +1,41 @@
+class Workouts {
+  constructor() {
+    this.workouts = [];
+    this.exercises = [];
+    this.adapter = new WorkoutsAdapter();
+    this.initBindingsAndEventListeners();
+    this.fetchAndLoadWorkouts();
+  }
+
+  initBindingsAndEventListeners() {}
+
+  createWorkout(e) {
+    e.preventDefault();
+    const value = this.newWorkoutName.value;
+
+    this.adapter.createWorkout(value).then((workout) => {
+      this.workouts.push(new Workout(workout));
+      this.newWorkoutName.value = "";
+      this.render();
+    });
+  }
+
+  fetchAndLoadWorkouts() {
+    this.adapter
+      .getWorkouts()
+      .then((workouts) => {
+        workouts
+          .sort((a, b) => a.id - b.id)
+          .forEach((workout) => this.workouts.push(new Workout(workout)));
+      })
+      .then(() => {
+        this.render();
+      });
+  }
+
+  render() {
+    this.workoutsContainer.innerHTML = this.workouts
+      .map((workout) => workout.renderLi())
+      .join("");
+  }
+}
