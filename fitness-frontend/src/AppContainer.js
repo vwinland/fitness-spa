0d5da6becd4561172bb1c0eb5a88c87ad23e7c36
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-05 17:02:10 -0400

    creates skeleton model inside appcontainer

--- fitness-frontend/src/AppContainer.js
+++ fitness-frontend/src/AppContainer.js
@@ -1,1 +1,13 @@
-class AppContainer {}
+class AppContainer {
+  workouts = [];
+  categories = [];
+  url = "";
+  workoutRoutine = {}; //fn that renders user routine after exercises added
+  getWorkouts() {
+    //make a fetch req to /workouts
+    //call renderWorkouts
+  }
+  renderWorkouts() {
+    //create DOM and insert data into them to append to the DOM
+  }
+}
