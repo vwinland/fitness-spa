98af9abe85bfaa5b650ef5903b5d9eed4eb4a814
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-04 23:50:19 -0400

    updates schema and seeds data

--- fitness-backend/db/migrate/20200505030320_create_workouts.rb
+++ fitness-backend/db/migrate/20200505030320_create_workouts.rb
@@ -2,7 +2,7 @@ class CreateWorkouts < ActiveRecord::Migration[6.0]
   def change
     create_table :workouts do |t|
       t.string :name
-      t.references :exercise, null: false, foreign_key: true
+      t.integer :exercise_id
 
       t.timestamps
     end
--- fitness-backend/db/seeds.rb
+++ fitness-backend/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+exercise1 = Exercise.create(name: "lunges")
+exercise2 = Exercise.create(name: "squats")
+exercise3 = Exercise.create(name: "pushups")
+
+Workout.create(name: "workout 1", exercise: exercise1)
+Workout.create(name: "workout 2", exercise: exercise2)
+Workout.create(name: "workout 3", exercise: exercise3)
