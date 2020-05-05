8e5799ad4c684b01ca9d24e002198436cd825552
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-05 17:04:01 -0400

    scultps out methods that will be needed for fetch request to api

--- fitness-frontend/src/AppContainer.js
+++ fitness-frontend/src/AppContainer.js
@@ -1,10 +1,11 @@
 class AppContainer {
   workouts = [];
-  categories = [];
+  exercises = [];
   url = "";
   workoutRoutine = {}; //fn that renders user routine after exercises added
   getWorkouts() {
     //make a fetch req to /workouts
+    //populate the activities and categories properties with the returned data
     //call renderWorkouts
   }
   renderWorkouts() {
