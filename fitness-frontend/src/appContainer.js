5975d908663f44187f8ed04bae2070164079cb82
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-05 19:35:14 -0400

    builds eventlisteners function for create workout routine button

--- fitness-frontend/src/appContainer.js
+++ fitness-frontend/src/appContainer.js
@@ -4,6 +4,10 @@ class AppContainer {
   url = "http://localhost:3000";
   workoutRoutine = {};
 
+  bindEventListeners() {
+    const btn = document.getElementById("createWorkoutRoutine");
+  }
+
   getWorkouts() {
     //make a fetch req to /workouts	    //make a fetch req to /workouts
     fetch(this.url + "/workouts")
