bcf9de81a35a75f7e0199d1db88ea90dc8680ed0
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-05 18:40:36 -0400

    build createWorkout for post request to backend

--- fitness-frontend/src/adapters/WorkoutsAdapter.js
+++ fitness-frontend/src/adapters/WorkoutsAdapter.js
@@ -1,6 +1,6 @@
 class WorkoutsAdapter {
   constructor() {
-    this.baseUrl = "http://localhost:3000/workouts";
+    this.baseUrl = "http://localhost:3000";
   }
 
   getWorkouts() {
@@ -9,3 +9,19 @@ class WorkoutsAdapter {
       .catch((err) => alert(err));
   }
 }
+createWorkout(value) {
+    const workout = {
+      name: value,
+    };
+
+    return fetch(this.baseUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ workout }),
+    }).then((res) => res.json());
+  }
+
+  
+  }
