173b795ce6eff287534bc46e7dc66a8d891d4283
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 16:00:05 -0400

    updates routes

--- fitness-backend/app/controllers/exercises_controller.rb
+++ fitness-backend/app/controllers/exercises_controller.rb
@@ -1,6 +1,12 @@
 class ExercisesController < ApplicationController
 
     def index 
-        render :json => Exercise.all
+        exercises = Exercise.all 
+        render json: ExerciseSerializer.new(exercises)
+    end
+
+    def show 
+        exercise = Exercise.find_by(id: params[:id])
+        render json: ExerciseSerializer.new(exercise).serialized_json
     end
 end
--- fitness-backend/config/routes.rb
+++ fitness-backend/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :exercises
+  resources :workouts do 
+    resources :exercises 
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #define routes
   ##namespace :api do
     ##namespace :v1 do
       
-      resources :workouts, :exercises
+      resources :excercises
     
   end
