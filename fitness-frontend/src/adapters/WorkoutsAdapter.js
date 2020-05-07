5f460e3a632d16710719d160092041c07d69c736
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-06 20:41:59 -0400

    adds fetch request to backend to make a post and patch request

--- fitness-frontend/src/adapters/WorkoutsAdapter.js
+++ fitness-frontend/src/adapters/WorkoutsAdapter.js
@@ -8,4 +8,32 @@ class WorkoutsAdapter {
       .then((res) => res.json())
       .catch((err) => alert(err));
   }
+
+  createWorkout(value) {
+    const workout = {
+      name: value,
+    };
+
+    return fetch(this.baseUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ workout }),
+    }).then((res) => res.json());
+  }
+
+  updateWorkout(value, id) {
+    const workout = {
+      name: value,
+    };
+
+    return fetch(`${this.baseUrl}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ workout }),
+    }).then((res) => res.json());
+  }
 }
