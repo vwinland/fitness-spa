a66f944a0f26b5901103a162dcbb079a332ff855
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 16:11:46 -0400

    adds create and destroy methods to exercise

--- fitness-backend/app/controllers/exercises_controller.rb
+++ fitness-backend/app/controllers/exercises_controller.rb
@@ -9,4 +9,24 @@ class ExercisesController < ApplicationController
         exercise = Exercise.find_by(id: params[:id])
         render json: ExerciseSerializer.new(exercise).serialized_json
     end
+
+    def create 
+        exercise = Exercise.find_by(id: params[:id])
+        if exercise.save
+            render json: ExerciseSerializer.new(exercise).serialized_json
+        end
+    end
+
+    def destroy 
+        exercise = Exercise.find_by(id: params[:id])
+        if exercise.destroy 
+            render josn: {message: "Exercise Deleted"}
+        end
+    end
+
+    private
+
+    def exercise_params
+        params.require(:exercise).permit(:name, :workout_id)
+    end
 end
