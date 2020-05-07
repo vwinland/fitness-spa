9396104ca1fbda626475291731f72a5b89981b80
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 16:07:17 -0400

    updates workouts controller with serializer

--- fitness-backend/app/controllers/workouts_controller.rb
+++ fitness-backend/app/controllers/workouts_controller.rb
@@ -1,42 +1,42 @@
 class WorkoutsController < ApplicationController
 
     def index 
-        render :json => Workout.all
+        workouts = Workout.all 
+        render json: WorkoutSerializer.new(workouts)
     end
 
-    
     def show 
-        @workout = Workout.find(params[:id])
+        workout = Workout.find_by(id: params[:id])
 
-        render json: @workout, status: 200
+        render json: WorkoutSerializer.new(workout).serialized_json
     end
 
-    def create #add in validation later
-        exercise = Exercise.find(params[:exercise].downcase)
+    # def create #add in validation later
+    #     exercise = Exercise.find(params[:exercise].downcase)
 
-        @workout = Workout.create(name: params[:name], exercise: exercise)
-        render json: @workout, status: 200
-    end
+    #     @workout = Workout.create(name: params[:name], exercise: exercise)
+    #     render json: @workout, status: 200
+    # end
 
-    def update #add in validation later
-        @workout = Workout.find(params[:id])
-        @workout.update(workout_params)
-        render json: @workout, status: 200
-    end
+    # def update #add in validation later
+    #     @workout = Workout.find(params[:id])
+    #     @workout.update(workout_params)
+    #     render json: @workout, status: 200
+    # end
 
-    def destroy
-        @workout = Workout.find(params[:id])
-        @workout.delete
+    # def destroy
+    #     @workout = Workout.find(params[:id])
+    #     @workout.delete
 
-        render json: {workoutId: @workout.id}
-    end
+    #     render json: {workoutId: @workout.id}
+    # end
 
 
 
-    #whitelist params
-    private  
-     def workout_params
-       params.require(:workout).permit(:name, :exercise_id)
-    end
+    # #whitelist params
+    # private  
+    #  def workout_params
+    #    params.require(:workout).permit(:name, :exercise_id)
+    # end
 end
 
