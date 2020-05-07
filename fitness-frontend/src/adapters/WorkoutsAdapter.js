61dedd791dc38e31450ac176805ea5dd2ddbdd0a
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 16:59:57 -0400

    adds getWorkouts function that makes fetch request to backend:

--- fitness-frontend/src/adapters/WorkoutsAdapter.js
+++ fitness-frontend/src/adapters/WorkoutsAdapter.js
@@ -2,4 +2,10 @@ class WorkoutsAdapter {
   constructor() {
     this.baseUrl = "http://localhost:3000/workouts";
   }
+
+  getWorkouts() {
+    return fetch(this.baseUrl)
+      .then((resp) => resp.json())
+      .then((json) => json.data);
+  }
 }
