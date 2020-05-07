a595a96d6ea9ed70501a730c0d2be08a147d6531
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-06 20:12:25 -0400

    adds baseUrl var to workoutsadapter and get workouts function

---
+++ fitness-frontend/src/adapters/WorkoutsAdapter.js
@@ -0,0 +1,11 @@
+class WorkoutsAdapter {
+    constructor() {
+      this.baseUrl = "http://localhost:3000/workouts";
+    }
+  
+    getWorkouts() {
+      return fetch(this.baseUrl)
+        .then((res) => res.json())
+        .catch((err) => alert(err));
+    }
+  
