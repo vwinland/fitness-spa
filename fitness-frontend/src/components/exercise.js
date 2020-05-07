0b307255d11f9335135a9d7142ed3818c70349c7
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 17:26:32 -0400

    adds create exercisecard function

--- fitness-frontend/src/components/exercise.js
+++ fitness-frontend/src/components/exercise.js
@@ -6,4 +6,20 @@ class Exercise {
     this.workout_name = workout_name;
     this.exercisesAdapter = new ExercisesAdapter();
   }
+
+  createExerciseCard() {
+    const container = document.querySelector(".container");
+    const exerciseCard = docuemtn.createElement("div");
+    exerciseCard.className = "exercise-card";
+    exerciseCard.setAttribute("data-id", `${this.id}`);
+    exerciseCard.setAttribute("data-workout-id", `${this.workout_id}`);
+    const exerciseName = document.createElement("div");
+    exerciseName.className = "exercise-name";
+    exerciseName.textContent = this.name;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-exercise";
+    deleteBtn.textContent = "Delete Workout";
+    deleteBtn.addEventListener("click", (e) => this.deleteItem(e));
+    container.append(exerciseCard);
+  }
 }
