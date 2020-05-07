763a63b71b898b6e97d3ccfddfa86da4c872b13f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-06 20:47:16 -0400

    adds CRUD to workouts controller

--- fitness-backend/app/controllers/workouts_controller.rb
+++ fitness-backend/app/controllers/workouts_controller.rb
@@ -3,4 +3,39 @@ class WorkoutsController < ApplicationController
     def index 
         render :json => Workout.all, :include => :exercise
     end
+
+    
+    def show 
+        @workout = Workout.find(params[:id])
+
+        render json: @workout, status: 200
+    end
+
+    def create #add in validation later
+    
+        @workout = Workout.create(workout_params)
+        render json: @workout, status: 200
+    end
+
+    def update #add in validation later
+        @workout = Workout.find(params[:id])
+        @workout.update(workout_params)
+        render json: @workout, status: 200
+    end
+
+    def destroy
+        @workout = Workout.find(params[:id])
+        @workout.delete
+
+        render json: {workoutId: @workout.id}
+    end
+
+
+
+    #whitelist params
+    private  
+     def workout_params
+       params.require(:workout).permit(:name, :exercise_id)
+    end
 end
+
