d6c84a0a1cb8fd7dcb48331dc21dc31b543c776b
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 16:58:42 -0400

    simplifies workouts adapter and adds baseurl

--- fitness-frontend/src/adapters/WorkoutsAdapter.js
+++ fitness-frontend/src/adapters/WorkoutsAdapter.js
@@ -2,38 +2,4 @@ class WorkoutsAdapter {
   constructor() {
     this.baseUrl = "http://localhost:3000/workouts";
   }
-
-  getWorkouts() {
-    return fetch(this.baseUrl)
-      .then((res) => res.json())
-      .catch((err) => alert(err));
-  }
-
-  createWorkout(value) {
-    const workout = {
-      name: value,
-    };
-
-    return fetch(this.baseUrl, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ workout }),
-    }).then((res) => res.json());
-  }
-
-  updateWorkout(value, id) {
-    const workout = {
-      name: value,
-    };
-
-    return fetch(`${this.baseUrl}/${id}`, {
-      method: "PATCH",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ workout }),
-    }).then((res) => res.json());
-  }
 }
