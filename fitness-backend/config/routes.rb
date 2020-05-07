283a52bc522db183ab88971602f5a8041ee85518
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 17:50:29 -0400

    fixes exercises resources route

--- fitness-backend/config/routes.rb
+++ fitness-backend/config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   resources :workouts do 
     resources :exercises 
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #define routes
-  ##namespace :api do
-    ##namespace :v1 do
-      
-    resources :excercises
+  resources :exercises
     
   end
