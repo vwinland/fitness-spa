6c564c7b14e09f7439d07ef1acbd2a4c147b975f
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-05-07 17:02:27 -0400

    adds getexercises function that makes fetch request to backend and render json

--- fitness-frontend/src/adapters/ExercisesAdapter.js
+++ fitness-frontend/src/adapters/ExercisesAdapter.js
@@ -2,4 +2,10 @@ class ExercisesAdapter {
   constructor() {
     this.baseUrl = "http://localhost:3000/exercises";
   }
+
+  getExercises() {
+    return fetch(this.baseUrl)
+      .then((resp) => resp.json())
+      .then((json) => json.data);
+  }
 }
